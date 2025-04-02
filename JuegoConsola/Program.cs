67fb8e0ffb56dc6f0e758f3796bd2ea8d891fe8f
dani <dani@DESKTOP-7B7EDP6>
2025-04-01 21:53:44 -0800

    Add project files.

---
+++ JuegoConsola/Program.cs
@@ -0,0 +1,73 @@
+﻿using JuegoConsola;
+using Microsoft.VisualBasic;
+using System.Drawing;
+using System.Reflection.PortableExecutable;
+
+//Ventana ventana = new Ventana(170,45,ConsoleColor.Black,new Point (5,5), new Point(165,40));
+//Ventana ventana = new Ventana(120, 30, ConsoleColor.Black, new Point(5, 5), new Point(115, 25));
+//ventana.DibujarMarco();
+Ventana ventana;
+Nave nave;
+bool jugar = true;
+bool bossFinal = false;
+Enemigo enemigo1;
+Enemigo enemigo2;
+Enemigo enemigoBoss;
+
+void Iniciar()
+{
+    ventana = new Ventana(120, 30, ConsoleColor.Black, new Point(5, 5), new Point(115, 25));
+    //ventana = new Ventana(180, 90, ConsoleColor.Black, new Point(5, 5), new Point(175, 60));
+    ventana.DibujarMarco();
+    nave = new Nave(new Point(60, 20), ConsoleColor.White, ventana);
+    nave.Dibujar();
+    enemigo1 = new Enemigo(new Point(50,10),ConsoleColor.Cyan,ventana,TipoEnemigo.Normal,nave);
+    enemigo1.Dibujar();
+     enemigo2 = new Enemigo(new Point(20, 12), ConsoleColor.Red, ventana, TipoEnemigo.Normal,nave);
+    enemigo2.Dibujar();
+    enemigoBoss = new Enemigo(new Point(100, 10), ConsoleColor.Magenta, ventana, TipoEnemigo.Boss,nave);
+    
+    nave.Enemigos.Add(enemigo1);
+    nave.Enemigos.Add(enemigo2);
+    nave.Enemigos.Add(enemigoBoss);
+
+
+}
+void Game()
+{
+    while (jugar)
+    {
+        if(!enemigo1.Vivo && !enemigo2.Vivo && !bossFinal)
+        {
+            bossFinal = true;
+            ventana.Peligro();
+        }
+        if (bossFinal)
+        {
+            enemigoBoss.Mover();
+            enemigoBoss.Informacion(100);
+        }
+        else
+        {
+            enemigo1.Mover();
+            enemigo1.Informacion(65);
+            enemigo2.Mover();
+            enemigo2.Informacion(83);
+        }
+        nave.Mover(1);
+        nave.Disparar();
+        //Thread.Sleep(50);
+        if (nave.Vida <= 0)
+        {
+            jugar = false;
+            nave.Muerte();
+        }
+        if (!enemigoBoss.Vivo)
+            jugar = false;
+
+    }
+}
+Iniciar();
+Game();
+
+//Console.ReadKey();
---
+++ JuegoConsola/Ventana.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoConsola
+{
+    class Ventana
+    {
+        public int Ancho { get; set; }
+        public int Altura { get; set; }
+        public ConsoleColor Color { get; set; }
+        public Point LimiteSuperior { get; set; }
+        public Point LimiteInferior { get; set; }
+
+        public Ventana(int Ancho, int Altura,ConsoleColor Color,Point LimiteSuperior, Point LimiteInferior)
+        {
+            this.Ancho = Ancho;
+            this.Altura = Altura;
+            this.Color = Color;
+            this.LimiteSuperior = LimiteSuperior;
+            this.LimiteInferior = LimiteInferior;
+            Init();
+        }
+        private void Init()
+        {
+            Console.SetBufferSize(Ancho, Altura);
+            Console.SetWindowSize(Ancho, Altura);
+            Console.CursorVisible = false;
+            Console.Title = "Nave";
+            Console.BackgroundColor = Color;
+            Console.Clear();
+        }
+        public void DibujarMarco()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = LimiteSuperior.X; i<= LimiteInferior.X; i++)
+            {
+                Console.SetCursorPosition(i, LimiteSuperior.Y);
+                Console.Write("═");
+                Console.SetCursorPosition(i, LimiteInferior.Y);
+                Console.Write("═");
+            }
+            for (int i = LimiteSuperior.Y; i <= LimiteInferior.Y; i++)
+            {
+                Console.SetCursorPosition(LimiteSuperior.X,i);
+                Console.Write("║");
+                Console.SetCursorPosition(LimiteInferior.X,i);
+                Console.Write("║");
+            }
+            Console.SetCursorPosition(LimiteSuperior.X, LimiteSuperior.Y);
+            Console.Write("╔");
+            Console.SetCursorPosition(LimiteSuperior.X, LimiteInferior.Y);
+            Console.Write("╚");
+            Console.SetCursorPosition(LimiteInferior.X, LimiteSuperior.Y);
+            Console.Write("╗");
+            Console.SetCursorPosition(LimiteInferior.X, LimiteInferior.Y);
+            Console.Write("╝");
+
+
+        }
+        public void Peligro()
+        {
+            Console.Clear();
+            DibujarMarco();
+            for (int i = 0; i < 6; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(LimiteInferior.X / 2 - 5, LimiteSuperior.Y / 2);
+                Console.Write("PELIGRO!!");
+                Thread.Sleep(200);
+                Console.SetCursorPosition(LimiteInferior.X / 2 - 5, LimiteSuperior.Y / 2);
+                Console.Write("            ");
+                Thread.Sleep(200);
+
+            }
+        }
+    }
+}
